0817a91a697c5841b59c8ed2d69372fa54d30beb
Etho <58352929+EthoIRL@users.noreply.github.com>
2022-09-23 01:32:58 -0700

    Implemented delete, and update username/password endpoints somewhat

--- backend/src/apis/user.rs
+++ backend/src/apis/user.rs
@@ -9,6 +9,7 @@ pub mod User {
         State, Responder
     };
     use serde::{Deserialize, Serialize};
+    use sled::IVec;
     use utoipa::{IntoParams, ToSchema};
 
     use pbkdf2::{
@@ -27,7 +28,7 @@ pub mod User {
         Unauthorized(String),
 
         #[response(status = 403)]
-        Forbidden(String),
+        Forbidden(Option<String>),
 
         #[response(status = 405)]
         NotAllowed(String),
@@ -77,7 +78,7 @@ pub mod User {
             .collect::<Vec<_>>();
         
         if users.iter().any(|user| user.username == username) {
-            return Err(Error::Forbidden(String::from("Username is already in use!")))
+            return Err(Error::Forbidden(Some(String::from("Username is already in use!"))))
         }
         
         let salt = SaltString::generate(&mut OsRng);
@@ -122,7 +123,7 @@ pub mod User {
     )]
     #[get("/login?<username>&<password>")]
     pub async fn login(
-        _config: &State<Arc<Mutex<Config>>>,
+        _config_store: &State<Arc<Mutex<Config>>>,
         database_store: &State<Arc<Mutex<sled::Db>>>,
         username: String,
         password: String
@@ -138,7 +139,7 @@ pub mod User {
                                     None => return Err(Error::InternalError(None))
                                 }
                             },
-                            Err(_) => return Err(Error::Forbidden(String::from("Password or username is not correct")))
+                            Err(_) => return Err(Error::Forbidden(Some(String::from("Password or username is not correct"))))
                         }
                     },
                     Err(_) => return Err(Error::InternalError(None))
@@ -163,7 +164,7 @@ pub mod User {
             Ok(_) => {
                 return Ok(user.api_key)
             },
-            Err(_) => return Err(Error::Forbidden(String::from("Password or username is not correct")))
+            Err(_) => return Err(Error::Forbidden(Some(String::from("Password or username is not correct"))))
         };
     }
 
@@ -171,52 +172,207 @@ pub mod User {
         get,
         context_path = "/user",
         responses(
-            (status = 200, description = "Successfully logged in account")
+            (status = 200, description = "Successfully deleted account"),
+            (status = 403, description = "An authentication issue has occured when attemping to delete an account", body = Error),
+            (status = 500, description = "An internal error on the server's end has occured", body = Error)
         )
     )]
     #[get("/delete?<username>&<password>")]
     pub async fn delete(
-        config: &State<Arc<Mutex<Config>>>,
-        database: &State<Arc<Mutex<sled::Db>>>,
+        _config_store: &State<Arc<Mutex<Config>>>,
+        database_store: &State<Arc<Mutex<sled::Db>>>,
         username: String,
         password: String
-    ) -> Status {
-        todo!()
+    ) -> Result<Status, Error> {
+        let user_database = match database_store.lock() {
+            Ok(database) => {
+                match database.open_tree("user") {
+                    Ok(result) => result,
+                    Err(_) => return Err(Error::InternalError(None))
+                }
+            },
+            Err(_) => return Err(Error::InternalError(Some(String::from("Failed to access backend database"))))
+        };
+
+        let user_vec = match user_database.get(&username) {
+            Ok(result) => {
+                match result {
+                    Some(result) => result,
+                    None => return Err(Error::InternalError(None))
+                }
+            },
+            Err(_) => return Err(Error::InternalError(Some(String::from("Couldn't find user associated with username"))))
+        };
+
+        let user: User = match serde_json::from_str(&String::from_utf8_lossy(&user_vec.to_vec())) {
+            Ok(result) => result,
+            Err(_) => return Err(Error::InternalError(None))
+        };
+
+        let password_hash = match PasswordHash::new(&user.password) {
+            Ok(result) => result,
+            Err(_) => return Err(Error::InternalError(None))
+        };
+
+        match Pbkdf2.verify_password(password.as_bytes(), &password_hash) {
+            Ok(_) => {
+                println!("Password verified");
+                match user_database.remove(username) {
+                    Ok(_) => {
+                        println!("Deleted account");
+                        return Ok(Status::Ok)
+                    },
+                    Err(_) => return Err(Error::InternalError(Some(String::from("Failed delete user account"))))
+                };
+            },
+            Err(_) => return Err(Error::Forbidden(None))
+        };
     }
 
     #[utoipa::path(
         get,
         context_path = "/user",
         responses(
-            (status = 200, description = "Successfully logged in account")
+            (status = 200, description = "Successfully updated account username"),
+            (status = 403, description = "An authentication issue has occured when attemping to update account username", body = Error),
+            (status = 500, description = "An internal error on the server's end has occured", body = Error)
         )
     )]
     #[get("/update/username?<username>&<password>&<new_username>")]
     pub async fn update_username(
-        config: &State<Arc<Mutex<Config>>>,
-        database: &State<Arc<Mutex<sled::Db>>>,
+        _config_store: &State<Arc<Mutex<Config>>>,
+        database_store: &State<Arc<Mutex<sled::Db>>>,
         username: String,
         password: String,
         new_username: String
-    ) -> Status {
-        todo!()
+    ) -> Result<Status, Error> {
+        let user_database = match database_store.lock() {
+            Ok(database) => {
+                match database.open_tree("user") {
+                    Ok(result) => result,
+                    Err(_) => return Err(Error::InternalError(None))
+                }
+            },
+            Err(_) => return Err(Error::InternalError(Some(String::from("Failed to access backend database"))))
+        };
+
+        let user_vec = match user_database.get(&username) {
+            Ok(result) => {
+                match result {
+                    Some(result) => result,
+                    None => return Err(Error::InternalError(None))
+                }
+            },
+            Err(_) => return Err(Error::InternalError(Some(String::from("Couldn't find user associated with username"))))
+        };
+
+        let mut user: User = match serde_json::from_str(&String::from_utf8_lossy(&user_vec.to_vec())) {
+            Ok(result) => result,
+            Err(_) => return Err(Error::InternalError(None))
+        };
+
+        let password_hash = match PasswordHash::new(&user.password) {
+            Ok(result) => result,
+            Err(_) => return Err(Error::InternalError(None))
+        };
+
+        return match Pbkdf2.verify_password(password.as_bytes(), &password_hash) {
+            Ok(_) => {
+                match user_database.remove(user.username) {
+                    Ok(_) => {
+                        // TODO: Make sure username is not already in use if throw error
+                        
+                        user.username = username.clone();
+                        
+                        let user_insert_vec = match serde_json::to_vec(&user) {
+                            Ok(result) => result,
+                            Err(_) => return Err(Error::InternalError(None))
+                        };
+
+                        match user_database.insert(new_username, user_insert_vec) {
+                            Ok(_) => {
+                                if let Err(_) = user_database.flush() {
+                                    return Err(Error::InternalError(Some(String::from("Failed to update backend database"))))
+                                };
+                                Ok(Status::Ok)
+                            },
+                            Err(_) => Err(Error::InternalError(None))
+                        }
+                    },
+                    Err(_) => Err(Error::InternalError(None)) 
+                }
+            },
+            Err(_) => Err(Error::Forbidden(None))
+        };
     }
 
     #[utoipa::path(
         get,
         context_path = "/user",
         responses(
-            (status = 200, description = "Successfully logged in account")
+            (status = 200, description = "Successfully updated account's password"),
+            (status = 403, description = "An authentication issue has occured when attemping to update account username", body = Error),
+            (status = 500, description = "An internal error on the server's end has occured", body = Error)
         )
     )]
     #[get("/update/password?<username>&<password>&<new_password>")]
     pub async fn update_password(
-        config: &State<Arc<Mutex<Config>>>,
-        database: &State<Arc<Mutex<sled::Db>>>,
+        _config_store: &State<Arc<Mutex<Config>>>,
+        database_store: &State<Arc<Mutex<sled::Db>>>,
         username: String,
         password: String,
         new_password: String
-    ) -> Status {
-        todo!()
+    ) -> Result<Status, Error> {
+        let user_database = match database_store.lock() {
+            Ok(database) => {
+                match database.open_tree("user") {
+                    Ok(result) => result,
+                    Err(_) => return Err(Error::InternalError(None))
+                }
+            },
+            Err(_) => return Err(Error::InternalError(Some(String::from("Failed to access backend database"))))
+        };
+
+        let user_vec = match user_database.get(&username) {
+            Ok(result) => {
+                match result {
+                    Some(result) => result,
+                    None => return Err(Error::InternalError(None))
+                }
+            },
+            Err(_) => return Err(Error::InternalError(Some(String::from("Couldn't find user associated with username"))))
+        };
+
+        let user: User = match serde_json::from_str(&String::from_utf8_lossy(&user_vec.to_vec())) {
+            Ok(result) => result,
+            Err(_) => return Err(Error::InternalError(None))
+        };
+
+        let password_hash = match PasswordHash::new(&user.password) {
+            Ok(result) => result,
+            Err(_) => return Err(Error::InternalError(None))
+        };
+
+        return match Pbkdf2.verify_password(password.as_bytes(), &password_hash) {
+            Ok(_) => {
+                match user_database.fetch_and_update(user.username, |option_vec| {
+                    let user_vec = option_vec.unwrap();
+                    let mut user: User = serde_json::from_str(&String::from_utf8_lossy(&user_vec.to_vec())).unwrap();
+                    // LOL... user.password = new_password.clone();
+                    // TODO:
+                    Some(IVec::from(serde_json::to_vec(&user).unwrap()))
+                }) {
+                    Ok(_) => {
+                        println!("changed password");
+                        if let Err(_) = user_database.flush() {
+                            return Err(Error::InternalError(Some(String::from("Failed to update backend database"))))
+                        };
+                        Ok(Status::Ok)
+                    },
+                    Err(_) => Err(Error::InternalError(None))
+                }
+            },
+            Err(_) => Err(Error::Forbidden(None))
+        };
     }
 }
