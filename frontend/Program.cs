3cf29e464feb57dc4bde625f97905d886399e52c
Etho <58352929+EthoIRL@users.noreply.github.com>
2023-01-22 14:12:26 -0800

    Never close connection to backend api
    This might come back to bite me in the ass later on but that's a later issue.

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -1,3 +1,4 @@
+using frontend.Api;
 using frontend.Config;
 using frontend.Web;
 
@@ -7,10 +8,12 @@ public class Program
 {
     public static ConfigManager ConfigManager;
     private static FrontendServer _frontend;
+    public static ApiUtils ApiUtils = null!;
     
     public static void Main(string[] args)
     {
         ConfigManager = new ConfigManager(Environment.CurrentDirectory);
+        new Thread(() => ApiUtils = new ApiUtils()).Start();
         new Thread(() => _frontend = new FrontendServer()).Start();
     }
 }
