2b51e5263225e920aacb9fc964e6130d7989ff0a
Etho <58352929+EthoIRL@users.noreply.github.com>
2023-02-03 01:09:42 -0800

    Index page layout and finished stats panel as well

--- frontend/Api/Models/User/ModelUserInfo.cs
+++ frontend/Api/Models/User/ModelUserInfo.cs
@@ -5,6 +5,7 @@ public class ModelUserInfo
     public string username { get; set; }
     public string creation_date { get; set; }
     public string[] uploads { get; set; }
+    public int total_upload_size { get; set; }
     public bool admin { get; set; }
     public string? invite_key { get; set; }
 }
