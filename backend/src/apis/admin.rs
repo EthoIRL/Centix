cae3b77be4e8fafb9fe90bf0b95b4fbdf085cfa2
Etho <58352929+EthoIRL@users.noreply.github.com>
2023-03-07 00:24:44 -0800

    Update admin.rs

--- backend/src/apis/admin.rs
+++ backend/src/apis/admin.rs
@@ -1,6 +1,16 @@
 #[allow(non_snake_case)]
 pub mod Admin {
 
-    // TODO:
+    // TODO: Implement Admin backend endpoints
+    
+    // Grabs a user's information such as when the account was created or the invite used.
+    // # admin/user/info/
+    // Grabs invite information such as who created the invite, who used the invite, when it was created & consumed
+    // # admin/user/invite/
+
+    // TODO: This should be incorporated into the media/user delete endpoints respectively
+    // Deletes a user's account
     // # admin/user/delete/
+    // Deletes a piece of media
+    // # admin/media/delete/
 }
