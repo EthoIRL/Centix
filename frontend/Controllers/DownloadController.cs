1975aedd7796e17731a26cc337e83da408496925
Etho <58352929+EthoIRL@users.noreply.github.com>
2023-03-21 23:35:57 -0700

    Simple post download & info page

---
+++ frontend/Controllers/DownloadController.cs
@@ -0,0 +1,26 @@
+﻿using frontend.Api.Models.Media;
+using frontend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace frontend.Controllers;
+
+public class DownloadController : Controller
+{
+    // TODO: Implement caching
+    [HttpGet("/download/")]
+    public async Task<ActionResult> Download([FromQuery] DownloadModel model)
+    {
+        ModelContentInfo? contentInfo = await Program.ApiUtils.GetAndReceiveModel<ModelContentInfo>(Program.ConfigManager.Config.BackendApiUri + String.Concat("/media/info?id=", model.id));
+        if (contentInfo != null)
+        {
+            var content = await Program.ApiUtils.GetAndReceiveByteArray(Program.ConfigManager.Config.BackendApiUri + String.Concat("/media/download?id=", model.id));
+            if (content != null)
+            {
+                var file = $"{contentInfo.content_name}.{contentInfo.content_extension}";
+                return File(content, "application/octet-stream", file);
+            }
+        }
+
+        return BadRequest();
+    }
+}
