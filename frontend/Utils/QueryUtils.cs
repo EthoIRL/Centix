86e02c85007e34814d3da164ba9f29e166229dd1
Etho <58352929+EthoIRL@users.noreply.github.com>
2023-03-25 22:24:21 -0700

    Implemented posts general sorting

---
+++ frontend/Utils/QueryUtils.cs
@@ -0,0 +1,23 @@
+﻿namespace frontend.Utils;
+
+public static class QueryUtils
+{
+    public static bool IsSelectedFromSortQuery(IQueryCollection queryCollection, String sortValue)
+    {
+        if (queryCollection.Count > 0)
+        {
+            foreach (var collection in queryCollection)
+            {
+                if (collection.Key.ToLower() == sortValue.ToLower())
+                {
+                    if (collection.Value[0] == null)
+                        continue;
+                    Boolean.TryParse(collection.Value[0], out bool value);
+                    return value;
+                }
+            }
+        }
+
+        return false;
+    }
+}
