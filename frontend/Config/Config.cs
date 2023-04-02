03c1a9a3f860870a861e31b18eb5b98ed10cc5d9
Etho <58352929+EthoIRL@users.noreply.github.com>
2023-04-01 18:17:57 -0700

    Implemented blurrable tags in the frontend config

--- frontend/Config/Config.cs
+++ frontend/Config/Config.cs
@@ -1,4 +1,7 @@
-﻿namespace frontend.Config;
+﻿using Newtonsoft.Json;
+using SixLabors.ImageSharp;
+
+namespace frontend.Config;
 
 public class Config
 {
@@ -11,4 +14,26 @@ public class Config
     /// </remarks>
     /// </summary>
     public string BackendApiUri { get; set; } = String.Empty;
+    
+    /// <summary>
+    /// Use transparency for padding or black bars for thumbnail images
+    /// </summary>
+    public bool TransparentThumbnailPadding { get; set; } = true;
+
+    /// <summary>
+    /// What thumbnails to blur based on tags
+    /// </summary>
+    public string[] BlurTags { get; set; } = {"nsfw"};
+    
+    /// <summary>
+    /// Text/HTML Snippet inserted into the announcement container
+    /// </summary>
+    /// <remarks>
+    /// Html A tag is supported
+    /// </remarks>
+    public string[] Announcements { get; set; } =
+    {
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. - Mar 30 2023",
+        @"This is an invite only service! (<a href=""https://google.com/"">https://google.com/</a>) - Mar 30 2023"
+    };
 }
