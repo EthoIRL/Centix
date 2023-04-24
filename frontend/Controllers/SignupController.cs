5daa141a84ab043a6e4e2353168a29bdfaaaaceb
Etho <58352929+EthoIRL@users.noreply.github.com>
2023-04-24 11:52:11 -0700

    Automatically log in when creating a new account

--- frontend/Controllers/SignupController.cs
+++ frontend/Controllers/SignupController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Net;
 using frontend.Api.Models;
 using frontend.Api.Models.User;
+using frontend.Api.Models.User.Login;
 using frontend.Models.Account;
+using frontend.Utils;
 using Microsoft.AspNetCore.Mvc;
 
 namespace frontend.Controllers;
@@ -30,15 +32,42 @@ public class SignupController : Controller
 
             if (response?.StatusCode == HttpStatusCode.OK)
             {
-                return Redirect("~/account");
+                var userLoginResponse =
+                    await Program.ApiUtils.PostAndReceiveResponse(
+                        Program.ConfigManager.Config.BackendApiUri + "/user/login", modelUserRegistration.user_credentials);
+
+                if (userLoginResponse?.StatusCode == HttpStatusCode.OK)
+                {
+                    var loginResponse = await userLoginResponse.Content.ReadFromJsonAsync<ModelLoginResponse>();
+
+                    if (loginResponse != null && loginResponse.key != String.Empty)
+                    {
+                        if (Request.Cookies[CookieUtils.CookieName] != null)
+                        {
+                            Response.Cookies.Delete(CookieUtils.CookieName);
+                        }
+
+                        Response.Cookies.Append(CookieUtils.CookieName, loginResponse.key, new CookieOptions
+                        {
+                            IsEssential = true,
+                            Secure = true,
+                            SameSite = SameSiteMode.Lax,
+                            Expires = DateTimeOffset.Now.AddDays(30)
+                        });
+                        
+                        return Redirect("~/account");
+                    }
+                }
             }
 
+            ModelError? error = null;
             if (response != null)
             {
-                var error = await response.Content.ReadFromJsonAsync<ModelError>();
-                ModelState.AddModelError("Error",
-                    error != null ? error.error : "An internal error has occured within the server!");
+                await response.Content.ReadFromJsonAsync<ModelError>();
             }
+
+            ModelState.AddModelError("Error", 
+                error != null ? error.error : "An internal error has occured within the server!");
         }
 
         if (ModelState.ErrorCount == 0)
