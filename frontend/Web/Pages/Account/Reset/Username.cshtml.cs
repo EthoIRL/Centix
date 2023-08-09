e52b11effaaa271da53c3e85e949e646a501f504
Etho <58352929+EthoIRL@users.noreply.github.com>
2023-08-08 23:12:29 -0700

    Added pages for account buttons

---
+++ frontend/Web/Pages/Account/Reset/Username.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace frontend.Web.Pages.Account.Reset;
+
+public class Username : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
